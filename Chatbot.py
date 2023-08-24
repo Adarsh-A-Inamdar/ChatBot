1e810560b07ed720e97a7b69b748c729a74a6fd5
Adarsh-Inamdar <120334374+Adarsh-Inamdar@users.noreply.github.com>
2023-08-24 11:54:06 +0530

    Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -14,12 +14,41 @@ pairs = [
         ["Sure, I can help with interview questions. Could you please specify the job role or industry you're interested in?"]
     ],
     [
-        r"(job role|industry) (.*)",
+        r"(job role|industry)",
         ["Great! For the {} job role in the {} industry, you can expect questions related to skills, experience, and behavioral scenarios. Would you like more specific questions?"]
     ],
     [
         r"tell me more|specific questions",
         ["Of course! Here are some common interview questions:\n1. Tell me about yourself.\n2. What are your strengths and weaknesses?\n3. Describe a challenging project you've worked on.\n4. Where do you see yourself in 5 years?\nRemember, it's essential to prepare specific examples and practice your responses."]
+    ],
+    #Questions on BIET'
+    [
+        r"Where is BIET",
+        ["Biet is located in Shamanur road,Davangere-577005"]
+    ],
+    [
+        r"Where is shiva temple in BIET",
+        ["It is located in infront of admin block"]
+    ],
+    [
+        r"What is the capacity of SSM Cultural hall ",
+        ["It has a Capacity of 1000 people "]
+    ],
+    [
+        r"What are the sports available to play in the campus",
+        ["The sports available are \n1.Cricket 2.Footbal \n3.batmintton \n4.Throwball \n5.Vollyball \n6.Carrom \n7.Chess"]
+    ],
+    [
+        r"What is the AIM of BIET|AIM|aim",
+        ["Building the budding generation with a noble mission of engineering a healthy and technologically developed society"]
+    ],
+    [
+        r"What is the VISION of BIET",
+        ["To be a centre of excellence recognized nationally and internationally, in distinctive areas of engineering education and research, based on a culture of innovation and invention"]
+    ],
+    [
+        r"What is the MISSION of BIET",
+        ["BIET contributes to the growth and development of its students by imparting a broad based engineering education and empowering them to be successful in their chosen field by inculcating in them positive approach, leadership qualities and ethical values"]
     ],
     [
         r"(.*)",
@@ -34,7 +63,6 @@ def chat_with_bot():
         user_input = input("You: ")
         if user_input.lower() == 'quit':
             print("Chatbot: Goodbye!")
-            break
         response = chatbot.respond(user_input)
         print("Chatbot:", response)
 if __name__ == "__main__":
