902e29aaf41d8cce0029c0d2ddfeef5e2b052611
Adarsh-Inamdar <120334374+Adarsh-Inamdar@users.noreply.github.com>
2023-08-24 08:59:24 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,41 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+pairs = [
+    [
+        r"hi|hello",
+        ["Hello!", "Hi there!", "How can I help you today?"]
+    ],
+    [
+        r"how are you",
+        ["I'm just a computer program, but I'm here to assist you.", "I don't have feelings, but thanks for asking!"]
+    ],
+    [
+        r"interview questions",
+        ["Sure, I can help with interview questions. Could you please specify the job role or industry you're interested in?"]
+    ],
+    [
+        r"(job role|industry) (.*)",
+        ["Great! For the {} job role in the {} industry, you can expect questions related to skills, experience, and behavioral scenarios. Would you like more specific questions?"]
+    ],
+    [
+        r"tell me more|specific questions",
+        ["Of course! Here are some common interview questions:\n1. Tell me about yourself.\n2. What are your strengths and weaknesses?\n3. Describe a challenging project you've worked on.\n4. Where do you see yourself in 5 years?\nRemember, it's essential to prepare specific examples and practice your responses."]
+    ],
+    [
+        r"(.*)",
+        ["We appreciate your inquiry. We would get back to you later with more information."]
+    ]
+    # Add more pattern-response pairs here
+]
+chatbot = Chat(pairs, reflections)
+def chat_with_bot():
+    print("Hello! I'm your chatbot. Type 'quit' to exit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'quit':
+            print("Chatbot: Goodbye!")
+            break
+        response = chatbot.respond(user_input)
+        print("Chatbot:", response)
+if __name__ == "__main__":
+    chat_with_bot()
